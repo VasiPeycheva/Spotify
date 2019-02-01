6df7799b4ca86af0887d9b6aa72de8c720b45213
VasiPeycheva <vasilena.peicheva@gmail.com>
2019-02-01 20:57:00 +0200

    added modifications for working with UsersDatabase

--- Spotify/src/bg/sofia/uni/fmi/mjt/server/ClientRequestHandler.java
+++ Spotify/src/bg/sofia/uni/fmi/mjt/server/ClientRequestHandler.java
@@ -6,6 +6,10 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
+import bg.sofia.uni.fmi.mjt.database.users.UsersDatabase;
+import bg.sofia.uni.fmi.mjt.database.users.exeptions.UserAlreadyExistException;
+import bg.sofia.uni.fmi.mjt.database.users.exeptions.UserNotRegisteredException;
+import bg.sofia.uni.fmi.mjt.database.users.exeptions.WrongPasswordException;
 import bg.sofia.uni.fmi.mjt.logger.Level;
 import bg.sofia.uni.fmi.mjt.logger.Logger;
 
@@ -14,9 +18,11 @@ public class ClientRequestHandler implements Runnable {
 	private PrintWriter write;
 	private BufferedReader read;
 	private Logger logger;
+	private UsersDatabase users;
 
-	public ClientRequestHandler(Socket s, Logger logger) {
+	public ClientRequestHandler(Socket s, Logger logger, UsersDatabase users) {
 		this.logger = logger;
+		this.users = users;
 		try {
 			read = new BufferedReader(new InputStreamReader(s.getInputStream()));
 		} catch (IOException e) {
@@ -31,15 +37,41 @@ public class ClientRequestHandler implements Runnable {
 
 	@Override
 	public void run() {
-		String request = null;
+		connect();
+	}
+
+	private void connect() {
 		try {
-			while ((request = read.readLine()) != null) {
-				write.println("server answer: >" + request);
+			String line = null;
+			while ((line = read.readLine()) != null) {
+				String[] token = line.split(" ");
+
+				if (token[0].equalsIgnoreCase("login")) {
+					try {
+						users.login(token[1], token[2]);
+						write.println("> Successfully logged in");
+						return;
+					} catch (UserNotRegisteredException | WrongPasswordException error) {
+						write.println(error.getMessage());
+					}
+				} else if (token[0].equalsIgnoreCase("register")) {
+					try {
+						users.register(token[1], token[2]);
+						write.println("> Successfully registered, please login");
+					} catch (UserAlreadyExistException error) {
+						write.println(error.getMessage());
+					}
+				} else if (token[0].equalsIgnoreCase("exit")) {
+					write.println("> Successfully exit");
+					return;
+				} else {
+					write.println("> Please REGISTER or LOGIN first");
+				}
+
 			}
 		} catch (IOException e) {
-			logger.log("unable to handle client request", Level.ERROR);
+			logger.log("unable to get client request", Level.ERROR);
 		}
-
 	}
 
 }
--- Spotify/src/bg/sofia/uni/fmi/mjt/server/Server.java
+++ Spotify/src/bg/sofia/uni/fmi/mjt/server/Server.java
@@ -3,11 +3,10 @@ package bg.sofia.uni.fmi.mjt.server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import bg.sofia.uni.fmi.mjt.database.users.UsersDatabase;
 import bg.sofia.uni.fmi.mjt.logger.FileLogger;
 import bg.sofia.uni.fmi.mjt.logger.Level;
 import bg.sofia.uni.fmi.mjt.logger.Logger;
@@ -17,10 +16,10 @@ public class Server {
 	private int PORT = 3333;
 	private int THREAD_NUMBER = 20;
 	private String LOG_FILENAME = "server_log.txt";
-	private Map<String, Socket> users;
 	private ServerSocket serverSocket;
 	private ExecutorService tasks;
 	private Logger logger;
+	private UsersDatabase users;
 
 	public Server() {
 		logger = new FileLogger(LOG_FILENAME);
@@ -32,7 +31,7 @@ public class Server {
 		}
 
 		tasks = Executors.newFixedThreadPool(THREAD_NUMBER);
-		users = new HashMap<>();
+		users = new UsersDatabase(logger);
 	}
 
 	public void start() {
@@ -40,7 +39,7 @@ public class Server {
 			try {
 				Socket s = serverSocket.accept();
 				logger.log("connected : user " + s.getInetAddress(), Level.INFO);
-				ClientRequestHandler request = new ClientRequestHandler(s, logger);
+				ClientRequestHandler request = new ClientRequestHandler(s, logger, users);
 				tasks.execute(request);
 			} catch (IOException e) {
 				logger.log("Failed to accept client connection", Level.ERROR);
